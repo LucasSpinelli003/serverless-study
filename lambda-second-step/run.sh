65125be7c21e5ac7e2c896be5cd453adaaf3ed57
LucasSpinelli003 <lucaszk003@gmail.com>
2024-11-15 17:52:54 -0300

    feature: imageAnalisys - implement lambda using rekognition and translate

--- lambda-second-step/run.sh
+++ lambda-second-step/run.sh
@@ -8,3 +8,16 @@ sls
 # sempre fazer um deploy do ambiente antes de tudo para verificar se está com o ambiente ok
 
 sls deploy
+
+#chamar a lambda na aws
+
+sls invoke -f <function_name>
+
+# chamar a lambda local
+
+sls invoke local -f <function_name>
+
+
+# ficar escutando uma lambda e seus logs
+
+sls logs -f <function_name> --tail
